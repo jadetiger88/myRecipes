6756fa31ae70b3a8f6678e4c9caabb48236a6bcc
jadetiger88 <jadetiger88@gmail.com>
2015-04-21 23:10:14 +0000

    Create and edit forms and partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravatar_for(chef, options = {size: 80})
+        gravarta_id = Digest::MD5::hexdigest(chef.email.downcase) 
+        size = options[:size]
+        gravata_url= "https://secure.gravatar.com/avatar/#{gravarta_id}?s=#{size}"
+        image_tag(gravata_url, alt: chef.chef_name, class: "gravartar")
+    end
 end
